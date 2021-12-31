27ecbd6da2bb3ce2ae9922041c8b6699e0334f5a
Randy Terrell <randy.l.terrell@gmail.com>
2021-12-31 14:19:46 -0700

    Example of getting data from Firebase in real time by using stream.

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -2,10 +2,7 @@ import 'package:cardpalz/widgets/header.dart';
 import 'package:cardpalz/widgets/progress.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-
 final usersRef = Firestore.instance.collection('users');
-
 
 class Timeline extends StatefulWidget {
   @override
@@ -15,25 +12,38 @@ class Timeline extends StatefulWidget {
 class _TimelineState extends State<Timeline> {
   @override
   void initState() {
-    getUsers();
+    // getUserById();
     super.initState();
   }
 
-  getUsers(){
-    usersRef.getDocuments().then((QuerySnapshot snapshot){
-      snapshot.documents.forEach((DocumentSnapshot doc) { 
-        print(doc.data);
-        print(doc.documentID);
-        print(doc.exists);
-      });
-    });
-  }
+  // getUserById() async {
+  //   final String id = "NdgXrzZy2kywY67PEkSw";
+  //   final DocumentSnapshot doc = await usersRef.document(id).get();
+  //   print(doc.data);
+  //   print(doc.documentID);
+  //   print(doc.exists);
+  // }
 
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: circularProgress(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: usersRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Text> children = snapshot.data.documents
+              .map((doc) => Text(doc['username']))
+              .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 }
